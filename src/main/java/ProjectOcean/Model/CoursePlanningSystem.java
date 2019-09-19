98d86863e8a733ba4d3134e8f617c1d9ee4b805b
Joel <joel.postonen@gmail.com>
2019-09-19 16:13:05 +0200

    Add: method returning all courses in CoursePlanningSystem class Co-authored by: Felix342<fillebraberg@gmail.com> Co-authored by: Br4xen <45483186+br4xen@users.noreply.github.com>

--- src/main/java/ProjectOcean/Model/CoursePlanningSystem.java
+++ src/main/java/ProjectOcean/Model/CoursePlanningSystem.java
@@ -1,4 +1,13 @@
 package ProjectOcean.Model;
 
+import java.util.List;
+
 public class CoursePlanningSystem {
+
+    private List<Course> courses;
+
+    public List<Course> getAllCourses() {
+        return courses;
+    }
+
 }
