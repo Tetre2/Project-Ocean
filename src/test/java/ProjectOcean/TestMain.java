66cc8a27158f0753b62f1047626cabd69fb77b1f
Eyzic <einar@ingemarsson.nu>
2019-09-19 13:49:24 +0200

    Add: testfile in the correct directory structure Add testfile in the correct structure according to maven.

---
+++ src/test/java/ProjectOcean/TestMain.java
@@ -0,0 +1,12 @@
+package ProjectOcean;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestMain{
+    @Test
+    public void testMain(){
+        Assert.assertTrue(1==1);
+    }
+}
