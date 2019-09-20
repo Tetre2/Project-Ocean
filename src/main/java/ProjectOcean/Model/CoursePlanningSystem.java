ce8f85849f09447ffc3f1256e58e590ce9c247eb
Felix342 <fillebraberg@gmail.com>
2019-09-20 14:41:48 +0200

    Add: test for the CoursePlanningSystem-class and created a it's constructor.

--- src/main/java/ProjectOcean/Model/CoursePlanningSystem.java
+++ src/main/java/ProjectOcean/Model/CoursePlanningSystem.java
@@ -6,6 +6,10 @@ public class CoursePlanningSystem {
 
     private List<Course> courses;
 
+    public CoursePlanningSystem() {
+        this.courses = new CourseInfo().getAllCourses();
+    }
+
     public List<Course> getAllCourses() {
         return courses;
     }
--- src/test/java/ProjectOcean/Model/CoursePlanningSystemTests.java
+++ src/test/java/ProjectOcean/Model/CoursePlanningSystemTests.java
@@ -1,4 +1,28 @@
 package ProjectOcean.Model;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
 public class CoursePlanningSystemTests {
+
+    @Test
+    public void CoursePlanningSystemTest() {
+        CoursePlanningSystem coursePlanningSystem = new CoursePlanningSystem();
+        CourseInfo courseInfo = new CourseInfo();
+
+        List<Course> coursePlanningSystemList = coursePlanningSystem.getAllCourses();
+        List<Course> courseInfoList= courseInfo.getAllCourses();
+
+        for(int i = 0; i < courseInfoList.size(); i++) {
+            Assert.assertEquals(courseInfoList.get(i).getName(), coursePlanningSystemList.get(i).getName());
+            Assert.assertEquals(courseInfoList.get(i).getCourseCode(), coursePlanningSystemList.get(i).getCourseCode());
+            Assert.assertEquals(courseInfoList.get(i).getStudyPoints(), coursePlanningSystemList.get(i).getStudyPoints(), 0);
+            Assert.assertEquals(courseInfoList.get(i).toString(), coursePlanningSystemList.get(i).toString());
+
+
+        }
+
+    }
 }
