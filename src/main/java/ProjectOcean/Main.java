35bcbd25fbb14a7550129a65b4dae591ea56c76d
Eyzic <einar@ingemarsson.nu>
2019-09-23 17:50:06 +0200

    Add: basic-gui and make program launch it from start Added a basic-gui with a view and a controller and made the program launch it when started.

--- src/main/java/ProjectOcean/Main.java
+++ src/main/java/ProjectOcean/Main.java
@@ -1,8 +1,17 @@
 package ProjectOcean;
 
-public class Main {
+import ProjectOcean.Controller.ApplicationController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Course Planning System");
+        ApplicationController root = new ApplicationController();
+        primaryStage.setScene(new Scene(root, 1200, 800));
+        primaryStage.show();
     }
 }
