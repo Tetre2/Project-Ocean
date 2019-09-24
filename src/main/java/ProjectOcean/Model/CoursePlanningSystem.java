d999f5260df93c6e379551c5f54fd9eac367c9a4
Br4xen <45483186+Br4xen@users.noreply.github.com>
2019-09-24 10:27:03 +0200

    Add: Implemented IModelCourseListIcon in CoursePlanningSystem

--- src/main/java/ProjectOcean/Model/CoursePlanningSystem.java
+++ src/main/java/ProjectOcean/Model/CoursePlanningSystem.java
@@ -1,8 +1,9 @@
 package ProjectOcean.Model;
 
 import java.util.List;
+import java.util.UUID;
 
-public class CoursePlanningSystem {
+public class CoursePlanningSystem implements IModelCourseListIcon{
 
     private List<Course> courses;
 
@@ -12,6 +13,34 @@ public class CoursePlanningSystem {
 
     public List<Course> getAllCourses() {
         return courses;
+    }
+
+    //Three methods that searches for course information based on UUID
+    public String getCourseCode(UUID id) {
+        for(Course c:courses){
+            if(c.getId() == id) {
+                return c.getCourseCode();
+            }
+        }
+        return "000-000";
+    }
+
+    public String getCourseName(UUID id) {
+        for(Course c:courses){
+            if(c.getId() == id) {
+                return c.getName();
+            }
+        }
+        return "No matching course id";
+    }
+
+    public String getCourseStudyPoints(UUID id) {
+        for(Course c:courses){
+            if(c.getId() == id) {
+                return c.getStudyPoints() + "";
+            }
+        }
+        return "0";
     }
 
 }
