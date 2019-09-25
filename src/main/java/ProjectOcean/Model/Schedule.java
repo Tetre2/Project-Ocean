8dc352aeebc26cd853a3c97f504a4d7e1122e4d6
Felix 342 <fillebraberg@gmail.com>
2019-09-25 19:11:36 +0200

    Add tests for the "Schedule"-class and implemented the methods for those tests

--- src/main/java/ProjectOcean/Model/Schedule.java
+++ src/main/java/ProjectOcean/Model/Schedule.java
@@ -1,8 +1,32 @@
 package ProjectOcean.Model;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class Schedule {
 
-    private List<Year> years;
+    private final List<Year> years = new ArrayList<>();
+
+    //A schedule starts with one year by default
+    public Schedule() {
+        years.add(new Year());
+    }
+
+
+    public void removeCourse(Course course, int year, int studyPeriod) {
+        years.get(year).getStudyPeriod(studyPeriod).removeCourse(course);
+    }
+
+
+    public void tryAddCourse(int year, int studyPeriod, int slot, Course course) {
+        years.get(year).getStudyPeriod(studyPeriod).addCourse(course, slot);
+    }
+
+
+    public Year getYear(int year){
+        return years.get(year);
+    }
+
 }
