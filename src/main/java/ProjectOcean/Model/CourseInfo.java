1ba39101e23d217b7df83a4aba0348a3567544f3
Br4xen <45483186+Br4xen@users.noreply.github.com>
2019-09-26 14:59:25 +0200

    Fix: Removed duplicate hard-coded courses

--- src/main/java/ProjectOcean/Model/CourseInfo.java
+++ src/main/java/ProjectOcean/Model/CourseInfo.java
@@ -14,21 +14,6 @@ public class CourseInfo implements InformationLoader {
         courses.add(new Course("MVE045","Matematisk Analys", 7.5f));
         courses.add(new Course("TMV206","Linjär Algebra", 7.5f));
         courses.add(new Course("TDA552","Objektorienterad Programmering och Design", 7.5f));
-        courses.add(new Course("DAT017","Maskinorienterad programmering", 7.5f));
-        courses.add(new Course("EDA433","Grundläggande Datorteknik", 7.5f));
-        courses.add(new Course("MVE045","Matematisk Analys", 7.5f));
-        courses.add(new Course("TMV206","Linjär Algebra", 7.5f));
-        courses.add(new Course("TDA552","Objektorienterad Programmering och Design", 7.5f));
-        courses.add(new Course("DAT017","Maskinorienterad programmering", 7.5f));
-        courses.add(new Course("EDA433","Grundläggande Datorteknik", 7.5f));
-        courses.add(new Course("MVE045","Matematisk Analys", 7.5f));
-        courses.add(new Course("TMV206","Linjär Algebra", 7.5f));
-        courses.add(new Course("TDA552","Objektorienterad Programmering och Design", 7.5f));courses.add(new Course("DAT017","Maskinorienterad programmering", 7.5f));
-        courses.add(new Course("EDA433","Grundläggande Datorteknik", 7.5f));
-        courses.add(new Course("MVE045","Matematisk Analys", 7.5f));
-        courses.add(new Course("TMV206","Linjär Algebra", 7.5f));
-        courses.add(new Course("TDA552","Objektorienterad Programmering och Design", 7.5f));
-
     }
 
     public List getAllCourses() {
