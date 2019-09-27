b364fa87d6d6a37b34a5b782bab6a512cf13b561
Joel <joel.postonen@gmail.com>
2019-09-27 10:57:09 +0200

    Add a graphical representation of a study plan and controller code to show the implementation when starting the app

--- src/main/java/ProjectOcean/Controller/StudyPlanController.java
+++ src/main/java/ProjectOcean/Controller/StudyPlanController.java
@@ -1,7 +1,34 @@
 package ProjectOcean.Controller;
 
-public class StudyPlanController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
+
+public class StudyPlanController extends VBox {
+
+    @FXML
+    private VBox yearContentView;
 
     private IModelStudyPlan model;
+    YearController yearController = new YearController();
+
+    public StudyPlanController() {
+        //ResourceBundle bundle = java.util.ResourceBundle.getBundle("Internationalization/Lang_sv");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+                "/ProjectOcean/View/StudyPlanWindow.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        yearContentView.getChildren().add(0, yearController);
+    }
 
 }
