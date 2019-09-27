ed1300aceaef5f38f4c6e6e9f9a9de3b83f1fadf
Felix 342 <fillebraberg@gmail.com>
2019-09-27 12:59:02 +0200

    Add tests for course adding methods in the "Student"-class and the implementations for these methods

--- src/main/java/ProjectOcean/Model/Student.java
+++ src/main/java/ProjectOcean/Model/Student.java
@@ -5,4 +5,29 @@ import java.util.List;
 public class Student {
 
     private List<ProjectOcean.Model.StudyPlan> studyPlans;
+    private StudyPlan currentStudyPlan;
+
+    public Student() {
+        this.currentStudyPlan = new StudyPlan();
+    }
+
+    public void addCourse(Course course, int year, int studyPeriod, int slot) {
+        currentStudyPlan.addCourseToSchedule(course, year, studyPeriod, slot);
+    }
+
+    public void removeCourse(Course course, int year, int studyPeriod) {
+        currentStudyPlan.removeCourseFromSchedule(course, year, studyPeriod);
+    }
+
+    public void addYear() {
+        currentStudyPlan.addYear();
+    }
+
+    public void removeYear(int year){
+        currentStudyPlan.removeYear(year);
+    }
+
+    public StudyPlan getCurrentStudyPlan() {
+        return currentStudyPlan;
+    }
 }
