53cbdcd3c2fa5d1ad167e7d94104589a9f7db783
Joel <joel.postonen@gmail.com>
2019-09-27 11:06:32 +0200

    Add a study plan controller to be created on start of the application and also add variable in Applications fxml to be able to insert a new year graphically

--- src/main/java/ProjectOcean/Controller/ApplicationController.java
+++ src/main/java/ProjectOcean/Controller/ApplicationController.java
@@ -1,12 +1,21 @@
 package ProjectOcean.Controller;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.VBox;
 
 public class ApplicationController extends VBox {
 
+    @FXML
+    private VBox contentWindow;
+
+    StudyPlanController studyPlanController = new StudyPlanController();
+
     public ApplicationController() {
+        //ResourceBundle bundle = java.util.ResourceBundle.getBundle("Internationalization/Lang_sv");
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
                 "/ProjectOcean/View/ApplicationWindow.fxml"));
         fxmlLoader.setRoot(this);
@@ -17,6 +26,8 @@ public class ApplicationController extends VBox {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+
+        contentWindow.getChildren().add(1, studyPlanController);
     }
 
 }
