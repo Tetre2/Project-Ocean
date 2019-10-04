d25dd4b663f02b4315f7dc3466dd72ba5916634a
Eyzic <einar@ingemarsson.nu>
2019-10-04 13:36:43 +0200

    Move instance variable workspace from CoursePlanningSystem to Student. Moved the instance of workspace in CoursePlanningSystem to Student so it matches the UML.

--- src/main/java/ProjectOcean/Model/Student.java
+++ src/main/java/ProjectOcean/Model/Student.java
@@ -1,11 +1,39 @@
 package ProjectOcean.Model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Represents a Student in the model
  */
 public class Student {
 
+    private Workspace workspace = new Workspace();
     private List<ProjectOcean.Model.StudyPlan> studyPlans;
+
+    /**
+     * Returns a list of courses that exists currently in the workspace.
+     * @return
+     */
+    public List<Course> getAllCoursesInWorkspace(){
+        return workspace.getAllCourses();
+    }
+
+    /**
+     * Adds a course to the students workspace
+     * @param course is the course to be added
+     */
+    public void addCourseToWorkspace(Course course){
+        workspace.addCourse(course);
+    }
+
+    /**
+     * Removes a course from the workspace
+     * @param course is the course to be added
+     */
+    public void removeCourseFromWorkspace(Course course) {
+        workspace.removeCourse(course);
+    }
+
 }
