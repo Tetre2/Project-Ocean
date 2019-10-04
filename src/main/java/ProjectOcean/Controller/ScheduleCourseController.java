a634625e2a7ddeb2b81616b40a535d4454e5258e
Felix 342 <fillebraberg@gmail.com>
2019-10-04 14:17:46 +0200

    Fix so that the studyplan gets filled with ScheduleCourseControllers instead of Labels

--- src/main/java/ProjectOcean/Controller/ScheduleCourseController.java
+++ src/main/java/ProjectOcean/Controller/ScheduleCourseController.java
@@ -1,11 +1,13 @@
 package ProjectOcean.Controller;
 
+import ProjectOcean.Model.CoursePlanningSystem;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  * Represents the visual graphic component of a course in study plan.
@@ -13,8 +15,11 @@ import java.io.IOException;
 public class ScheduleCourseController extends VBox {
 
     @FXML private Label courseCodeLabel;
+    private final CoursePlanningSystem model;
+    private final UUID id;
 
-    public ScheduleCourseController() {
+    public ScheduleCourseController(CoursePlanningSystem model, UUID id) {
+
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
                 "/ScheduleCourseView.fxml"));
@@ -26,6 +31,10 @@ public class ScheduleCourseController extends VBox {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+
+        this.model = model;
+        this.id = id;
+        this.courseCodeLabel.setText(model.getCourse(id).getCourseCode());
     }
 
 }
