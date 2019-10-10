a77de282e5c698fbbe4ff46ad112899d4b1b94c4
Tetre2 <mattias.retteli@gmail.com>
2019-10-10 14:46:23 +0200

    add javaDoc to interfaces

--- src/main/java/ProjectOcean/IO/ICourseSaveLoader.java
+++ src/main/java/ProjectOcean/IO/ICourseSaveLoader.java
@@ -7,6 +7,10 @@ import java.util.UUID;
 
 public interface ICourseSaveLoader {
 
+    /**
+     * tries to load all courses form a file if it cant load it it creates a new empty file
+     * @returns the loaded courses
+     */
     Map<UUID, Course> tryToLoadCoursesFileIfNotCreateNewFile();
 
 }
--- src/main/java/ProjectOcean/IO/IStudyPlanSaverLoader.java
+++ src/main/java/ProjectOcean/IO/IStudyPlanSaverLoader.java
@@ -4,8 +4,16 @@ import ProjectOcean.Model.Student;
 
 public interface IStudyPlanSaverLoader {
 
+    /**
+     * Saves the students studyplans to a file
+     * @param student
+     */
     void saveStudyplans(Student student);
 
+    /**
+     * tries to load a student form a file if it cant load it it creates a new empty file
+     * @returns the loaded student
+     */
     Student tryToLoadStudentFileIfNotCreateNewFile();
 
 
