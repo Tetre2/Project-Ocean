d056c662f131eb4a6c832ef829d02fc6362cc56c
Tetre2 <mattias.retteli@gmail.com>
2019-10-10 13:36:44 +0200

    refactor java.util* import replaced with the actual imports used

--- src/main/java/ProjectOcean/IO/CoursesSaverLoader.java
+++ src/main/java/ProjectOcean/IO/CoursesSaverLoader.java
@@ -10,7 +10,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+import java.util.Map;
 
 public class CoursesSaverLoader implements ICourseSaveLoader{
 
